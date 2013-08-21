3805be895e68320b8d7e99793b20c21d7049b1dc
Marcus Cavanaugh <m@mcav.com>
2013-08-21 09:54:20 -0700

    Adds plaintext escaping functions.
    bleach.escapePlaintextIntoElementContext neutralizes all HTML characters;
    bleach.escapePlaintextIntoAttribute renders text safe for inserting into an attribute.

---
+++ test/test-escape-plaintext.js
@@ -0,0 +1,25 @@
+var mocha = require('mocha')
+  , bleach = require('../')
+  , should = require('should');
+
+describe('bleach', function () {
+  describe('escaping', function () {
+    it('should escape plaintext for elements', function() {
+      bleach.escapePlaintextIntoElementContext([
+        '<p>a <strong>"simple"',
+        '</strong> example</p>'
+      ].join('\n')).should.equal([
+        '&lt;p&gt;a &lt;strong&gt;&quot;simple&quot;',
+        '&lt;&#47;strong&gt; example&lt;&#47;p&gt;'
+      ].join('\n'));
+    });
+    
+    it('should escape plaintext for attributes', function() {
+      bleach.escapePlaintextIntoAttribute([
+        'try">\'to escape'
+      ].join('\n')).should.equal([
+        'try&#34;&#62;&#39;to&#32;escape'
+      ].join('\n'));
+    });
+  });
+});
